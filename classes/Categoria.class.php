756636b5a343107f908b8d49f7fc53e47ed619c2
RyanCarmo <103540552+RyanCarmo@users.noreply.github.com>
2023-04-13 21:39:52 -0300

    Add files via upload
    Criação das classes

---
+++ classes/Categoria.class.php
@@ -0,0 +1,15 @@
+<?php
+class Categoria{
+    protected $id;
+    protected $nome;
+
+
+    public function setNome($n){
+        $this->nome = $n;
+    }
+    public function getNome(){
+        return $this->nome;
+
+    }
+}
+?>
